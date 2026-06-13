ebcfd4282a021b2bc66d92748b488c8642c5eb16
Виолетта <enchenkova52@yandex.ru>
2026-06-14 00:00:28 +0300

    Update Bonus.h

--- Arcanoid/Bonus.h
+++ Arcanoid/Bonus.h
@@ -1,21 +1,45 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <memory>
+
+class Paddle;
+class Ball;
 
 class Bonus {
-private:
+protected:
     sf::RectangleShape shape;
-    float speed;
-    bool active;
+    float speed = 150.f;
+    bool active = true;
 
 public:
     Bonus(float x, float y);
+    virtual ~Bonus() = default;
 
-    void update(float deltaTime);
+    virtual void apply(Paddle& paddle, Ball& ball, bool& bottomShield) = 0;
+
+    void update(float dt);
     void draw(sf::RenderWindow& window) const;
 
     sf::FloatRect getBounds() const;
-
     bool isActive() const;
     void deactivate();
+};
+
+class PaddleSizeBonus : public Bonus {
+public:
+    PaddleSizeBonus(float x, float y);
+    void apply(Paddle& paddle, Ball& ball, bool& bottomShield) override;
+};
+
+class BottomShieldBonus : public Bonus {
+public:
+    BottomShieldBonus(float x, float y);
+    void apply(Paddle& paddle, Ball& ball, bool& bottomShield) override;
+};
+
+class RandomDirectionBonus : public Bonus {
+public:
+    RandomDirectionBonus(float x, float y);
+    void apply(Paddle& paddle, Ball& ball, bool& bottomShield) override;
 };
